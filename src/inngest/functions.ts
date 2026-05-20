cf0b8c3b645553cdb14ef4e064b3b4b60aae3517
imTanush02 <tanushsingh843@gmail.com>
2026-05-20 11:42:39 +0530

    now i know what e2b is made for

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,19 @@
 // src/inngest/functions.ts
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 
 export const processTask = inngest.createFunction(
   { id: "process-task", triggers: { event: "app/task.created" } },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id" , async () => {
+      const sandbox = await Sandbox.create({
+        template: "tanushsingh843/genie-nextjs-test2"
+      })
+      return sandbox.sandboxId
+    })
     const codeAgent= createAgent({
       name: 'codeAgent',
       system: "you are a code agent who generate the code for the prompt given to you",
@@ -14,7 +22,13 @@ export const processTask = inngest.createFunction(
     const { output } = await codeAgent.run(`generate code for this prompt : ${event.data.value}`);
     console.log(output[0].content);
 
-    return { message: `code generated : ${output[0].content}` };
+    const sandboxUrl = await step.run("get-sandbox-url" , async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host =  await sandbox.getHost(3000);
+      return `https://${host}`
+    });
+
+    return { output , sandboxUrl };
   }
 );
 
