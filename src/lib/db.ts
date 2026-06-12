6c041a19da680bf893a77c45b1474029700c1e0c
imTanush02 <tanushsingh843@gmail.com>
2026-06-12 12:39:50 +0530

    inngest fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,9 +3,10 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient; 
 }; 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL, 
-}); 
+import { Pool } from "pg";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool); 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
